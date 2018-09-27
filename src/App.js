f43157ed23eb191d62a271d1bff467a7598f5c84
Kaccady <z-rz@mail.com>
2018-09-27 16:51:27 +0300

    before

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import Canvas from './components/Canvas';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <p>{this.props.message}</p>
-      </div>
+     <Canvas/>
     );
   }
 }
-App.propTypes = {
-  message: PropTypes.string.isRequired,
-};
+
 
 export default App;
