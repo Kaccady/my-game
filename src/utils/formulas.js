3ac73f4574793e88d9fcdb9b37b048305e0393de
Kaccady <z-rz@mail.com>
2018-10-03 17:28:06 +0300

    its work

--- src/utils/formulas.js
+++ src/utils/formulas.js
@@ -9,7 +9,9 @@ export const pathFromBezierCurve = (cubicBezierCurve) => {
             ${endingAxis.x} ${endingAxis.y}
             `;
     };
+
 export const radiansToDegress = radians => ((radians * 180)/Math.PI);
+
 export const calculateAngle = (x1, y1, x2, y2) => {
     if (x2 >=0 && y2 >=0) {
         return-90;
@@ -19,12 +21,13 @@ export const calculateAngle = (x1, y1, x2, y2) => {
     const quotient = dividend/divisor;
     return radiansToDegress(Math.atan(quotient)) *-1;
 };
+
 export const getCanvasPosition = (event) => {
     const svg = document.getElementById('aliens-go-home-canvas');
     const point = svg.createSVGPoint();
 
     point.x = event.clientX;
     point.y = event.clientY;
-    const { x, y } = point.matrixTransform(svg.getScreenCTM().inrerverse());
+    const { x, y } = point.matrixTransform(svg.getScreenCTM().inverse());
     return {x,y};
 };
